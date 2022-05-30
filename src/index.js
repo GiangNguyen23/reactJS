e3f256c8cf584b560489527ac01187e7efd03d1b
GiangNguyen23 <giang.nguyen23@student.passerellesnumeriques.org>
2022-05-30 11:04:58 +0700

    Misterm

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Admin from "./Misterm/Left/Admin";
 
-// import News from "./Misterm/Left/Above";
-// import News1 from "./Misterm/Left/Below";
-// import Ads from "./Misterm/Left/Ads";
+import News from "./Misterm/Left/Above";
+import News1 from "./Misterm/Left/Below";
+import Ads from "./Misterm/Left/Ads";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <div className="content">
-  //   <div className="left">
-  //     <News></News>
-  //     <News1></News1>
-  //   </div>
-  //   <div className="right">
-  //     <Ads></Ads>
-  //   </div>
-  // </div>
-  <div className="container">
-    <Admin />
+  <div className="content">
+    <div className="left">
+      <News></News>
+      <News1></News1>
+    </div>
+    <div className="right">
+      <Ads></Ads>
+    </div>
   </div>
+  // <div className="container">
+  //   <Admin />
+  // </div>
 );
